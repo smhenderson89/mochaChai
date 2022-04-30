c4592f458c2d6263c485e91ff23f4b46f2b4cf3f
Scott Henderson <scott.mc.henderson@gmail.com>
2022-04-29 21:04:56 -0700

    QA_functionalTest#3

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -57,9 +57,27 @@ suite('Functional Tests', function () {
     });
     // #4
     test('Send {surname: "da Verrazzano"}', function (done) {
-      assert.fail();
-
-      done();
+      chai
+        .request(server)
+        .put('/travellers')
+        .send({
+          "surname" : "da Verrazzano"
+        })
+        .end(function (err, res) {
+          assert.equal(res.status, 200, 'response is 200');
+          assert.equal(res.type, 'application/json', 'type is JSON');
+          assert.equal(
+            res.body.name,
+            'Giovanni',
+            'res.body.name is Giovanni'
+          );
+          assert.equal(
+            res.body.surname,
+            'da Verrazzano',
+            'res.body.surname is da Verrazzano'
+          );
+          done();
+        });
     });
   });
 });
